3ebc09918472f529714190ac3697f77f38881c79
HJXIcon <x1248399884@163.com>
2017-07-25 11:04:52 +0800

    1.构建项目基本框架
    构建项目基本框架

---
+++ JXLovePlayNews/JXLovePlayNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JXLovePlayNews
+//
+//  Created by mac on 17/7/25.
+//  Copyright © 2017年 JXIcon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JXLovePlayNews/JXLovePlayNews/Modeuls/Common/Common.h
@@ -0,0 +1,16 @@
+//
+//  Common.h
+//  LovePlayNews
+//
+//  Created by mac on 17/7/25.
+//  Copyright © 2017年 JXIcon. All rights reserved.
+//
+
+#ifndef Common_h
+#define Common_h
+
+// ios8以上
+#define kIsIOS8Later ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+
+
+#endif /* Common_h */
